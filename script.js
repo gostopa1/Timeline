39e43fe165aaf87a2fedd051ae8e4101797206c9
Athanasios Gotsopoulos <tanilas@hotmail.com>
2024-04-26 01:55:49 +0300

    Dynamic element with popup done!

--- script.js
+++ script.js
@@ -4,18 +4,25 @@ var fullHeight = window.innerHeight;
 
 //var text = this.querySelector("input[type='text']").value;
 var text = "Xontrompigoulis"
-//var elem = document.createElement("div");
-var elem = document.createElement("p");
-//elem.setAttribute("id", "rcorners1");
-elem.setAttribute("class", "p2");
+var elem = document.createElement("div");
+
+elem.setAttribute("id", "rcorners1");
+elem.setAttribute("class", "popup");
+
+var ahu = document.createElement("span");
+ahu.setAttribute("class", "popuptext");
+ahu.setAttribute("id", "myPopup");
+//ahu.textContent="fritzioui";
+ahu.innerHTML="fritzioui";
+
 
 elem.textContent = text;
 elem.style.position = "absolute";
 elem.style.left = Math.round(Math.random() * fullWidth) + "px";
 elem.style.top = Math.round(Math.random() * fullHeight) + "px";
 elem.style.width = Math.round(Math.random()*100 + 10) + "px";
-
-
+elem.appendChild(ahu);
+//document.body.appendChild(ahu);
 document.body.appendChild(elem);
 var positionInfo = elem.getBoundingClientRect();
 var height = positionInfo.height;
