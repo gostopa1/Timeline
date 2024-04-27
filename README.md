aac37e193b18f810d7d33ccd8745f886ac045e4c
Athanasios Gotsopoulos <tanilas@hotmail.com>
2024-04-28 00:32:07 +0300

    Added events with timestamps

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ An online tool to visualize a timeline based on list of events with popup suppor
 ### Items to implement
 
 * [x] Different color per category
-* [ ] Convert date timestamp to pixels and populate events with dates
+* [x] Convert date timestamp to pixels and populate events with dates
 * [ ] Make legend for categories?
 * [ ] Hide all other popups when clicking another?
 * [ ] Center text in events?
--- script.js
+++ script.js
@@ -48,17 +48,20 @@ function getPixelFromDate(inputDate)
     return output;
 }
 
+var colors = ["#73AD21","#73FDAA","#0000FF","#00FF00"];
+
 var EventList = 
 [
 TimelineEvent.create(1, "2005-09-01", "2011-12-11", "Diploma in Computer Engineering","University of Patras"), 
 TimelineEvent.create(2, "2013-09-01", "2017-06-01", "Bachelor in Music Technology","Sibelius Academy"), 
-TimelineEvent.create(3, "2010-09-01", "2024-01-29", "PhD in Computational Neuroscience","Aalto University",2), 
-TimelineEvent.create(4, "2016-07-04", "2016-12-15", "Military Service", "Vekaranjarvi"),
-TimelineEvent.create(5, "2016-10-25", "2094-12-15", "A baby was born", "Filippos", 2),
-TimelineEvent.create(5, "2016-10-25", "2094-12-15", "A baby was born", "Filippos", 1),
+TimelineEvent.create(3, "2010-09-01", "2024-01-29", "PhD in Computational Neuroscience","Aalto University"), 
+TimelineEvent.create(4, "2016-07-04", "2016-12-15", "Military Service", "Vekaranjarvi",1),
+TimelineEvent.create(5, "2023-10-25", "2094-12-15", "A baby was born", "Filippos", 2),
+TimelineEvent.create(1, "2018-01-08", "2094-12-15", "Neural DSP","Aalto University",3), 
+TimelineEvent.create(7, "2023-05-13", "2023-05-13", "AES presentation","Machine Learning for Guitar Amplifier modeling",3), 
 ]
 
-var colors = ["#73AD21","#73FDAA","#0000FF"];
+
 
 function addTimelineEvent(timelineEvent)
 {
