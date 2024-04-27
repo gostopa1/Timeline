414d2a8f43b945334b90b07d69ebc63944e22d0a
Athanasios Gotsopoulos <tanilas@hotmail.com>
2024-04-28 01:46:47 +0300

    Moved data to another file, added end date to correspond to 'today'

--- script.js
+++ script.js
@@ -2,35 +2,9 @@
 var fullWidth = window.innerWidth;
 var fullHeight = window.innerHeight;
 
-var TimelineEvent = {
-    startingDate: 0,
-    endingDate: 100,
-    importance: 1,
-    yLocation: 1,
-    text: "PhD",
-    category: 1,
-    popupText: "I did this and that",
-    create: function (eventYLocation=1,startingDate, endingDate, eventText = "An Event",eventPopuptext = "This is what happened", eventCategory = 0)
-    {
-      const newTimelineEvent = Object.create(this);
-      newTimelineEvent.startingDate = startingDate;
-      newTimelineEvent.endingDate = endingDate;
-      newTimelineEvent.text = eventText;
-      newTimelineEvent.popupText = eventPopuptext;
-      newTimelineEvent.category = eventCategory;
-      newTimelineEvent.yLocation = eventYLocation*50+500;
-      return newTimelineEvent;
-    },
-    'setYLocation': function (newYLocation)
-    {
-        console.log("Got in here");
-        this.yLocation=newYLocation;
-        console.log(this.yLocation);
-    }
-};
-
 var startingDate = "2010-01-01";
-var endDate = "2024-07-01";
+//var endDate = "2024-07-01";
+var endDate = today;
 //var exampleDate = "2024-07-01T20:23:01.804Z"
 //var date = "2012-01-01"
 
